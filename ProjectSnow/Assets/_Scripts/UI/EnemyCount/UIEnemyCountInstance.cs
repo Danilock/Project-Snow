d9b8153bb6cbfd923538efd2d79654aa63bbef01
Daniel Ercilio Del Rosario Guerra <yo1234re@gmail.com>
2022-03-12 02:39:58 -0400

    Added new enemies art and cleaned project

--- ProjectSnow/Assets/_Scripts/UI/EnemyCount/UIEnemyCountInstance.cs
+++ ProjectSnow/Assets/_Scripts/UI/EnemyCount/UIEnemyCountInstance.cs
@@ -1,21 +1,16 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 using DG.Tweening;
 using TMPro;
+using UnityEngine.UI;
 
 namespace Game.UI
 {
     public class UIEnemyCountInstance : MonoBehaviour
     {
-        public UIEnemyCountInstance PreviousCount;
-        public UIEnemyCountInstance NextCount;
-        public TMP_Text TMProText;
-
-        public void SetSize(Vector3 newSize, float duration)
-        {
-            transform.DOScale(newSize, duration);
-        }
+        
     }
 }
